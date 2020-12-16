e11237c28401f156f8482e62c84e3becae1535cd
weijiew <836678589@qq.com>
2020-12-16 13:11:15 +0800

    delete

--- component---src-pages-about-js-390ad1fbd81383213fc3.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"3XHS":function(n,t,a){"use strict";a.r(t);a("q1tI");var o=a("aArQ"),c=a("qKvR");t.default=function(n){var t=n.location;return Object(c.a)(o.a,{location:t},Object(c.a)("h1",null,"A student!"),Object(c.a)("p",null,"Life is good."))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-390ad1fbd81383213fc3.js.map
